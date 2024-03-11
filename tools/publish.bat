4ca3c0a1a666b9a5738b3ef10c8ad8a173a26d5c
cabiste <ahmedjj354@gmail.com>
2024-03-11 14:34:29 +0100

    cleaned the publish tool

--- tools/publish.bat
+++ tools/publish.bat
@@ -1,25 +1,33 @@
-echo off
+@echo off
 
-mkdir publish publish\windows publish\linux publish\mac
+mkdir publish
 
-rmdir /s /q tdl-wrapper\obj tdl-wrapper\bin
 :: Windows
-echo publishing windows with bflat
-dotnet build tdl-wrapper\tdl-wrapper.csproj
-bflat build -Os --no-globalization --os windows --arch x64 -o publish\windows\tdl-wrapper_Windows_64bit.exe
-bflat build -Os --no-globalization --os windows --arch arm64 -o publish\windows\tdl-wrapper_Windows_arm64.exe
-
-echo publishing windows with dotnet
-dotnet publish tdl-wrapper\tdl-wrapper.csproj --sc -p:PublishSingleFile=true -p:InvariantGlobalization=true -p:PublishTrimmed=true -c release -p:AssemblyName=tdl-wrapper_Windows_32bit -r win-x86 -o publish\windows
+powershell write-host -fore Cyan "Compiling for Windows"
 
 rmdir /s /q tdl-wrapper\obj tdl-wrapper\bin
+dotnet build tdl-wrapper\tdl-wrapper.csproj
+
+bflat build -Os --no-globalization --os windows --arch x64 -o publish\tdl-wrapper_Windows_64bit.exe
+bflat build -Os --no-globalization --os windows --arch arm64 -o publish\tdl-wrapper_Windows_arm64.exe
+
+dotnet publish tdl-wrapper\tdl-wrapper.csproj --sc -p:PublishSingleFile=true -p:InvariantGlobalization=true -p:PublishTrimmed=true -c release -p:AssemblyName=tdl-wrapper_Windows_32bit -r win-x86 -o publish
+
 :: Linux
-echo publishing Linux with bflat
-dotnet build tdl-wrapper\tdl-wrapper.csproj
-bflat build -Os --separate-symbols --no-globalization --os linux --arch x64 -o publish\linux\tdl-wrapper_Linux_64bit
-bflat build -Os --separate-symbols --no-globalization --os linux --arch arm64 -o publish\linux\tdl-wrapper_Linux_arm64
+powershell write-host -fore Cyan "Compiling for Linux"
 
 rmdir /s /q tdl-wrapper\obj tdl-wrapper\bin
+dotnet build tdl-wrapper\tdl-wrapper.csproj
+
+bflat build -Os --separate-symbols --no-globalization --os linux --arch x64 -o publish\tdl-wrapper_Linux_64bit
+bflat build -Os --separate-symbols --no-globalization --os linux --arch arm64 -o publish\tdl-wrapper_Linux_arm64
+
 :: MacOs
-dotnet publish tdl-wrapper\tdl-wrapper.csproj --sc -p:PublishSingleFile=true -p:InvariantGlobalization=true -p:PublishTrimmed=true -c release -p:AssemblyName=tdl-wrapper_MacOs_64bit -r osx-x64 -o publish\mac
-dotnet publish tdl-wrapper\tdl-wrapper.csproj --sc -p:PublishSingleFile=true -p:InvariantGlobalization=true -p:PublishTrimmed=true -c release -p:AssemblyName=tdl-wrapper_MacOs_arm64 -r osx-arm64 -o publish\mac
+powershell write-host -fore Cyan "Compiling for MacOs"
+rmdir /s /q tdl-wrapper\obj tdl-wrapper\bin
+
+dotnet publish tdl-wrapper\tdl-wrapper.csproj --sc -p:PublishSingleFile=true -p:InvariantGlobalization=true -p:PublishTrimmed=true -c release -p:AssemblyName=tdl-wrapper_MacOs_64bit -r osx-x64 -o publish
+dotnet publish tdl-wrapper\tdl-wrapper.csproj --sc -p:PublishSingleFile=true -p:InvariantGlobalization=true -p:PublishTrimmed=true -c release -p:AssemblyName=tdl-wrapper_MacOs_arm64 -r osx-arm64 -o publish
+
+powershell write-host -fore Cyan "Finished compiling, deleting artifacts..."
+DEl publish\*.pdb publish\*.dwo
